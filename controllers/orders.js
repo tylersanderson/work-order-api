b846c334f591100faf85bedf58bc608d4aaa3e66
tylersanderson <tylersanderson@hotmail.com>
2019-12-14 07:56:41 -0600

    sample order reset handler added

--- controllers/orders.js
+++ controllers/orders.js
@@ -24,10 +24,42 @@ const handleOrderUpdate = (req, res, db) => {
 				res.status(400).json('Unable to update')
 			}
 		})
-		.catch(err => res.status(400).json('error updating user'))
+		.catch(err => res.status(400).json('error updating order'))
+}
+
+const handleOrderReset = (req, res, db) => {
+	
+		db('orders').where({ ordernumber: 100121 }).update({ ordernotes: "100121 notes...", isordercomplete: false })
+		.then()	
+		db('orders').where({ ordernumber: 100122 }).update({ ordernotes: "100122 notes...", isordercomplete: false })
+		.then()
+		db('orders').where({ ordernumber: 100123 }).update({ ordernotes: "100123 notes...", isordercomplete: false })
+		.then()
+		db('orders').where({ ordernumber: 100124 }).update({ ordernotes: "100124 notes...", isordercomplete: false })
+		.then()	
+		db('orders').where({ ordernumber: 100125 }).update({ ordernotes: "100125 notes...", isordercomplete: false })
+		.then()
+		db('orders').where({ ordernumber: 100126 }).update({ ordernotes: "100126 notes...", isordercomplete: false })
+		.then()
+		db('orders').where({ ordernumber: 100127 }).update({ ordernotes: "100127 notes...", isordercomplete: false })
+		.then()	
+		db('orders').where({ ordernumber: 100128 }).update({ ordernotes: "100128 notes...", isordercomplete: false })
+		.then()
+		db('orders').where({ ordernumber: 100129 }).update({ ordernotes: "100129 notes...", isordercomplete: false })
+		.then()
+		db('orders').where({ ordernumber: 100130 }).update({ ordernotes: "100130 notes...", isordercomplete: false })
+		.then(resp => {
+			if (resp) {
+				res.json("success")
+			} else {
+				res.status(400).json('Unable to update')
+			}
+		})
+		.catch(err => res.status(400).json('error updating orders'))
 }
 
 module.exports = {
 	handleOrdersGet,
-	handleOrderUpdate
+	handleOrderUpdate,
+	handleOrderReset
 }
--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -2,9 +2,16 @@ BEGIN TRANSACTION;
 
 INSERT into users (name, email, entries, joined) values ('Tyler', 'tyler@gmail.com', 5, '2018-01-01');
 INSERT into login (hash, email) values ('$2a$10$upz0hE5YgrSIiHOsRKwqeuPNGJ1A3GvPVTEvcK/mOxKay30YK8CRa', 'tyler@gmail.com');
-INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100122', '221B Baker St', 'Sherlock Holmes is Loud', 'Order Notes for 221B', false);
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100121', '221A Baker St', 'Tenant above is loud, bad at violin', 'Order Notes for 221A', false);
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100122', '221B Baker St', 'Sherlock Holmes wants new neighbors', 'Order Notes for 221B', false);
 INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100123', '221C Baker St', 'Cranky about noisy neighbor', 'Order Notes for 221C', false);
-INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100124', '1000 Main St', 'This guy is has an issue', 'Order Notes for 1000 Main St.', false);
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100124', '1001 Main St', 'Repairs needed to the flux capacitor', 'Order Notes for 1001 Main St.', false);
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100125', '1002 Main St', 'Customer requests more blinker fluid', 'Order Notes for 1002 Main St.', false);
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100126', '1003 Main St', 'Doc Brown having trouble with DeLorean', 'Order Notes for 1003 Main St.', false);
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100127', '1004 Main St', 'Voice box repairs needed for Mr. Vader', 'Order Notes for 1004 Main St.', false);
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100128', '1005 Main St', 'Mr. Hut requests a new Sarlacc pit', 'Order Notes for 1005 Main St.', false);
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100129', '1006 Main St', 'Jetpack requires maintenance for Boba', 'Order Notes for 1006 Main St.', false);
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100130', '1007 Main St', 'Repairs needed, there are for Mr. Yoda', 'Order Notes for 1007 Main St.', false);
 
 
 COMMIT;
