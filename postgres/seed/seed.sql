407381bee2e354db91ad48f3bdb4aba6ac3b005e
tylersanderson <tylersanderson@hotmail.com>
2019-12-24 07:03:17 -0600

    map component and seed data complete

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -2,16 +2,16 @@ BEGIN TRANSACTION;
 
 INSERT into users (name, email, entries, joined) values ('Tyler', 'tyler@gmail.com', 5, '2018-01-01');
 INSERT into login (hash, email) values ('$2a$10$upz0hE5YgrSIiHOsRKwqeuPNGJ1A3GvPVTEvcK/mOxKay30YK8CRa', 'tyler@gmail.com');
-INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100121', '221A Baker St', 'Tenant above is loud, bad at violin', 'Order Notes for 221A', false);
-INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100122', '221B Baker St', 'Sherlock Holmes wants new neighbors', 'Order Notes for 221B', false);
-INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100123', '221C Baker St', 'Cranky about noisy neighbor', 'Order Notes for 221C', false);
-INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100124', '1001 Main St', 'Repairs needed to the flux capacitor', 'Order Notes for 1001 Main St.', false);
-INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100125', '1002 Main St', 'Customer requests more blinker fluid', 'Order Notes for 1002 Main St.', false);
-INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100126', '1003 Main St', 'Doc Brown having trouble with DeLorean', 'Order Notes for 1003 Main St.', false);
-INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100127', '1004 Main St', 'Voice box repairs needed for Mr. Vader', 'Order Notes for 1004 Main St.', false);
-INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100128', '1005 Main St', 'Mr. Hut requests a new Sarlacc pit', 'Order Notes for 1005 Main St.', false);
-INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100129', '1006 Main St', 'Jetpack requires maintenance for Boba', 'Order Notes for 1006 Main St.', false);
-INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete) values ('100130', '1007 Main St', 'Repairs needed, there are for Mr. Yoda', 'Order Notes for 1007 Main St.', false);
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete, orderLat, orderLong) values ('100121', 'One Memorial Stadium Drive', 'Maintenance requested on stadium screens', 'Order Notes for 100121...', false, '40.8206', '-96.7056');
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete, orderLat, orderLong) values ('100122', '1445 K St', 'Sower statue requires renovation', 'Order Notes for 100122...', false, '40.8081', '-96.6997');
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete, orderLat, orderLong) values ('100123', '1222 S 27th St', 'Zoo animals escaped', 'Order Notes for 100123...', false, '40.8006', '-96.6795');
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete, orderLat, orderLong) values ('100124', '1600 S 48th St', 'Hospital heliport needs resurfaced', 'Order Notes for 100124...', false, '40.47451800', '-96.39071700');
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete, orderLat, orderLong) values ('100125', '2300 S 16th St', 'Repairs need to the flux capacitor', 'Order Notes for 100125...', false, '40.790185', '-96.698142');
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete, orderLat, orderLong) values ('100126', '6100 O St', 'Doc Brown having trouble with 1985 DeLorean', 'Order Notes for 100126...', false, '40.815737', '-96.636109');
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete, orderLat, orderLong) values ('100127', '555 S 70th St', 'Voice box repairs needed for Mr. Vader', 'Order Notes for 100127...', false, '40.808101', '-96.627339');
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete, orderLat, orderLong) values ('100128', '3201 S Coddington Ave', 'Mr. Hut requests a new Sarlacc pit', 'Order Notes for 100128...', false, '40.774238', '-96.777671');
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete, orderLat, orderLong) values ('100129', '2021 Transformation Dr', 'Jetpack requires maintenance for Boba', 'Order Notes for 100129...', false, '40.832757', '-96.692750');
+INSERT into orders (orderNumber, address, description, orderNotes, isOrderComplete, orderLat, orderLong) values ('100130', '12000 West Branched Oak Rd', 'Repairs needed, there are. Requested by Mr. Yoda', 'Order Notes for 100130...', false, '40.972434', '-96.869421');
 
 
 COMMIT;
--- postgres/tables/orders.sql
+++ postgres/tables/orders.sql
@@ -3,9 +3,11 @@ BEGIN TRANSACTION;
 CREATE TABLE orders (
 	orderNumber serial PRIMARY KEY,
 	address VARCHAR(100),
-	description VARCHAR(40),
+	description VARCHAR(50),
 	orderNotes VARCHAR(500),
-	isOrderComplete boolean
+	isOrderComplete boolean,
+	orderLat NUMERIC(15,13),
+	orderLong NUMERIC(15,13)
 );
 
 COMMIT;
